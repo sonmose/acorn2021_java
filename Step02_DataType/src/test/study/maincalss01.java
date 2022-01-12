999707609f94c28dd0df543413661755a34fafaf
SONMOSE <sonmose529@naver.com>
2022-01-12 12:03:02 +0900

    test.packge added

--- Step02_DataType/src/test/study/maincalss01.java
+++ Step02_DataType/src/test/study/maincalss01.java
@@ -3,8 +3,15 @@ package test.study;
 public class maincalss01 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		System.out.println("main 메소스가 실행되었습니다");
+		byte a=10;
+		short b=10;
+		int c=10;
+		long d=10;
+		
+		float e=10.1f;
+		double f=10.1d;
+		double g=10.1;
 	}
 
 }
