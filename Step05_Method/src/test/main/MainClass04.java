f8e3945dc638632019f76756490da92a78943ce2
SONMOSE <sonmose529@naver.com>
2022-01-13 16:23:15 +0900

    Step 05 MainClass04에 주석 added

--- Step05_Method/src/test/main/MainClass04.java
+++ Step05_Method/src/test/main/MainClass04.java
@@ -6,6 +6,7 @@ import test.mypac.Phone;
 public class MainClass04 {
 	public static void main(String[] args) {
 		//MyObject 클래스에 있는 usePhone() 메소드를 호출해 보세요.
+		//m1에 참조값을 담아놔서 여러번 사용가능하다
 		MyObject m1=new MyObject();
 		Phone p1=new Phone();
 		m1.usePhone(p1);
@@ -15,7 +16,7 @@ public class MainClass04 {
 		m2.usePhone(new Phone());
 		
 		System.out.println("- 위와 동일한 작업을 1 줄의 코딩으로 한다면?");
-		
+		//1회용 한번사용가능
 		new MyObject().usePhone(new Phone());
 	}
 }
