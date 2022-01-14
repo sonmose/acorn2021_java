629c4f9a8194d97b167e32c625fc30e4584d8036
SONMOSE <sonmose529@naver.com>
2022-01-14 11:13:59 +0900

    file mkdir,delete added

--- Step05_Method/src/test/main/MainClass01.java
+++ Step05_Method/src/test/main/MainClass01.java
@@ -18,12 +18,34 @@ public class MainClass01 {
 	    //리턴되는 값을 b 라는 이름의 지역 변수에 담아 보세요.
 		String b=m1.getGreeting();
 		
+		/*
+		 * java 에서 file를 삭제 하고 싶다.
+		 * 파일 삭제를 하는 메소드를 가지고 있는 클래스는 뭐지?
+		 * java.io.File 클래스
+		 * 1.import
+		 * 2.객체 생성 (참조값 얻어내기)
+		 * 3.메소드 생성
+	
 		File f1=new File("C:\\acorn202112\\test01");
 		boolean isSuccess=f1.delete();
 		if(isSuccess) {
 			System.out.println("삭제성공");
 		}else {
 			System.out.println("삭제실패");
+		}
+		*/
+		
+		/*
+		 * f1.mkdir();
+		 * f1.delete();
+		 * test01 가 존재하면 삭제하고, 존재하지 않으면 만드는 프로그래밍을 해보세요.
+		 */
+		File f1=new File("C:\\acorn202112\\test01");
+		boolean isExists=f1.exists();
+		if(isExists) {
+			f1.delete();
+		}else {
+			f1.mkdir();
 		}
 	}
 }
