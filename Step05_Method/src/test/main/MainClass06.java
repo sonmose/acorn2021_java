f3cda9053cea5d506b6f908baff2338a30df7a1a
SONMOSE <sonmose529@naver.com>
2022-01-14 11:50:28 +0900

    MainClass06 fix

--- Step05_Method/src/test/main/MainClass06.java
+++ Step05_Method/src/test/main/MainClass06.java
@@ -6,8 +6,10 @@ public class MainClass06 {
 	public static void main(String[] args) {
 		Random ran=new Random();
 		//1~45사이의 랜덤한 정수를 하나 얻어내서 콘솔창에 출력하는 프로그래밍을 해보세요.
-		int num=ran.nextInt(45);
+		for(int i=0; i<100; i++) {
+		int num=ran.nextInt(45)+1;
 		System.out.println(num);
+		}
 		
 	}
 
